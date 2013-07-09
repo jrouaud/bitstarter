2f66bc84cccb3d34632a202ce513e2816b1942ed
jrouaud <ubuntu@ip-172-31-44-241.us-west-2.compute.internal>
2013-07-09 14:55:26 +0000

    Fix web.js

--- web.js
+++ web.js
@@ -5,7 +5,10 @@ var data = fs.readFileSync("index.html");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(data);
- {
+  response.send(data.toString('utf-8'));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
   console.log("Listening on " + port);
 });
