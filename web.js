3cbde611c50577622b56c5f27114a0b4ea1c3887
jrouaud <ubuntu@ip-172-31-44-241.us-west-2.compute.internal>
2013-07-09 14:49:25 +0000

    Modifs on web.js

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
+var data = fs.readFileSync("index.html");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+  response.send(data);
+ {
   console.log("Listening on " + port);
 });
